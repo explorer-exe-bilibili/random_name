99d8cf3ec6892d1def95af6f84d103e83b27cea4
_exe explorer <j13879066396@163.com>
2024-11-10 23:46:01 +0800

    add files

---
+++ files/imgs/webp to bmp.py
@@ -0,0 +1,29 @@
+﻿import os
+import glob
+from PIL import Image
+
+def convert_webp_to_bmp(directory, output_directory):
+    # 确保输出目录存在
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+    # 获取目录下所有的WebP文件
+    webp_files = glob.glob(os.path.join(directory, '*.webp'))
+
+    # 遍历所有WebP文件
+    for webp_file in webp_files:
+        # 获取文件名和扩展名
+        file_name, file_ext = os.path.splitext(os.path.basename(webp_file))
+        # 创建新的BMP文件名
+        bmp_file = os.path.join(output_directory, file_name + '.bmp')
+
+        # 打开WebP文件
+        with Image.open(webp_file) as img:
+            # 保存为BMP格式
+            img.save(bmp_file, 'BMP')
+
+# 使用示例
+input_directory = '.'  # 替换为你的输入目录路径
+output_directory = '.\\out'  # 替换为你想要保存BMP文件的目录路径
+
+convert_webp_to_bmp(input_directory, output_directory)
